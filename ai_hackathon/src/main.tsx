c27c8047dd812421a981289d77870b7ee5f8e692
MaxRoenco <maxroenco2004@gmail.com>
2025-01-25 18:54:56 +0200

    exstension

--- ai_hackathon/src/main.tsx
+++ ai_hackathon/src/main.tsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Check if running in extension context
+const rootElement = document.getElementById('extension-root') || document.getElementById('root');
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
